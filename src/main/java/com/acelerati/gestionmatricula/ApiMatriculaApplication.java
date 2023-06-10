01256ec3770eeeb23804f5815a3e7f901de006be
Nelson M <nelsondmondragon@gmail.com>
2023-06-10 11:58:44 -0500

    primera version api-matricula

---
+++ src/main/java/com/acelerati/gestionmatricula/ApiMatriculaApplication.java
@@ -0,0 +1,13 @@
+package com.acelerati.gestionmatricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMatriculaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acelerati/gestionmatricula/ApiMatriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acelerati.gestionmatricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMatriculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
